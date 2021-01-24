c07e30a675e39ca641ab6b17ebd8791d40dd4d45
Kris Zyp <kriszyp@gmail.com>
2021-01-23 17:22:29 -0700

    Use external-string based extraction

--- src/extract.cpp
+++ src/extract.cpp
@@ -23,59 +23,156 @@ using namespace v8;
 #endif
 #endif
 
+const size_t UTF16_SPACE = 0x4000;
+
+class CustomExternalStringResource : public String::ExternalStringResource {
+private:
+    const uint16_t *d;
+
+public:
+    CustomExternalStringResource() {
+    // The Latin data
+    this->d = new uint16_t[UTF16_SPACE];
+    // Number of Latin characters in the string
+}
+    ~CustomExternalStringResource() {};
+
+	void Dispose() {
+	    delete this;
+	}
+	const uint16_t *data() const {
+	    return this->d;
+	}
+
+    size_t length() const {
+	    return UTF16_SPACE;
+	}
+
+};
+class CustomExternalOneByteStringResource : public String::ExternalOneByteStringResource {
+private:
+    const char *d;
+    size_t l;
+
+public:
+    CustomExternalOneByteStringResource(char *data, size_t size) {
+    // The Latin data
+    this->d = data;
+    // Number of Latin characters in the string
+    this->l = size;
+}
+    ~CustomExternalOneByteStringResource() {};
+
+	void Dispose() {
+	    delete this;
+	}
+    const char *data() const{
+	    return this->d;
+	}
+
+    size_t length() const {
+	    return this->l;
+	}
+
+};
+
 const int MAX_TARGET_SIZE = 255;
 typedef int (*token_handler)(uint8_t* source, int position, int size);
 token_handler tokenTable[256] = {};
 class Extractor {
 public:
-	v8::Local<v8::Value> target[MAX_TARGET_SIZE + 1]; // leave one for the queued string
-
 	uint8_t* source;
 	int position = 0;
-	int writePosition = 0;
+	int writeSizePosition = 0;
 	int stringStart = 0;
 	int lastStringEnd = 0;
+	uint32_t sizes[0x80];
+	CustomExternalStringResource utf16Resource;
+	uint16_t* utf16Position;
+	CustomExternalOneByteStringResource* utf8Resource;
 	Isolate *isolate = Isolate::GetCurrent();
 	void readString(int length, bool allowStringBlocks) {
 		int start = position;
 		int end = position + length;
-		if (allowStringBlocks) { // for larger strings, we don't bother to check every character for being latin, and just go right to creating a new string
-			while(position < end) {
-				if (source[position] < 0x80) // ensure we character is latin and can be decoded as one byte
-					position++;
-				else {
-					break;
-				}
+		while(position < end) {
+			if (source[position] < 0x80) // ensure we character is latin and can be decoded as one byte
+				position++;
+			else {
+				position = start;
+				break;
 			}
 		}
 		if (position < end) {
-			// non-latin character
 			if (lastStringEnd) {
-				target[writePosition++] = String::NewFromOneByte(isolate,  (uint8_t*) source + stringStart, v8::NewStringType::kNormal, lastStringEnd - stringStart).ToLocalChecked();
+				sizes[writeSizePosition++] = lastStringEnd;
 				lastStringEnd = 0;
 			}
-			// use standard utf-8 conversion
-			target[writePosition++] = Nan::New<v8::String>((char*) source + start, (int) length).ToLocalChecked();
-			position = end;
-			return;
-		}
-
-		if (lastStringEnd) {
-			if (start - lastStringEnd > 40 || end - stringStart > 4000) {
-				target[writePosition++] = String::NewFromOneByte(isolate, (uint8_t*) source + stringStart, v8::NewStringType::kNormal, lastStringEnd - stringStart).ToLocalChecked();
-				stringStart = start;
+			// non-latin character(s), need conversion from utf-8 to utf-16
+			uint16_t* startPosition = utf16Position;
+			while(position < end) {
+				uint8_t byte1 = source[position++];
+				if ((byte1 & 0x80) == 0) {
+					// 1 byte
+					*utf16Position = byte1;
+					utf16Position++;
+				} else if ((byte1 & 0xe0) == 0xc0) {
+					// 2 bytes
+					uint8_t byte2 = source[position++] & 0x3f;
+					*utf16Position = ((byte1 & 0x1f) << 6) | byte2;
+					utf16Position++;
+				} else if ((byte1 & 0xf0) == 0xe0) {
+					// 3 bytes
+					uint8_t byte2 = source[position++] & 0x3f;
+					uint8_t byte3 = source[position++] & 0x3f;
+					*utf16Position = ((byte1 & 0x1f) << 12) | (byte2 << 6) | byte3;
+					utf16Position++;
+				} else if ((byte1 & 0xf8) == 0xf0) {
+					// 4 bytes
+					uint8_t byte2 = source[position++] & 0x3f;
+					uint8_t byte3 = source[position++] & 0x3f;
+					uint8_t byte4 = source[position++] & 0x3f;
+					uint32_t unit = ((byte1 & 0x07) << 0x12) | (byte2 << 0x0c) | (byte3 << 0x06) | byte4;
+					if (unit > 0xffff) {
+						unit -= 0x10000;
+						*utf16Position = ((unit >> 10) & 0x3ff) | 0xd800;
+						utf16Position++;
+						unit = 0xdc00 | (unit & 0x3ff);
+					}
+					*utf16Position = unit;
+					utf16Position++;
+				} else {
+					*utf16Position = byte1;
+				}
 			}
-		} else {
-			stringStart = start;
+			sizes[writeSizePosition++] = utf16Position - startPosition; // record the length in the sizes array
+			position = end; // ensure that we are aligned
+			return;
 		}
 		lastStringEnd = end;
 	}
 
-	Local<Value> extractStrings(int startingPosition, int size, uint8_t* inputSource) {
-		writePosition = 0;
-		lastStringEnd = 0;
-		position = startingPosition;
+	Local<String> setupUtf16() {
+		return String::NewExternalTwoByte(isolate, &utf16Resource).ToLocalChecked();
+	}
+
+	Local<Value> setupSizesBuffer() {
+		return Nan::NewBuffer((char*)sizes, 0x200,
+			[](char *, void *) {
+				// Don't need to free it
+			}, nullptr).ToLocalChecked();
+	}
+
+	Local<Value> setupUtf8(uint8_t* inputSource, size_t size) {
 		source = inputSource;
+		utf8Resource = new CustomExternalOneByteStringResource((char*) inputSource, size);
+		return String::NewExternalOneByte(isolate, utf8Resource).ToLocalChecked();
+	}
+
+	void extractStrings(int startingPosition, int size) {
+		writeSizePosition = 0;
+		lastStringEnd = 0;
+		utf16Position = (uint16_t*) utf16Resource.data();
+		position = startingPosition;
 		while (position < size) {
 			uint8_t token = source[position++];
 			if (token < 0xa0) {
@@ -85,17 +182,17 @@ public:
 				token -= 0xa0;
 				if (token + position > size) {
 					Nan::ThrowError("Unexpected end of buffer reading string");
-					return Nan::Null();
+					return;
 				}
 				readString(token, true);
-				if (writePosition >= MAX_TARGET_SIZE)
+				if (writeSizePosition >= MAX_TARGET_SIZE)
 					break;
 			} else if (token <= 0xdb && token >= 0xd9) {
 				if (token == 0xd9) { //str 8
 					int length = source[position++];
 					if (length + position > size) {
 						Nan::ThrowError("Unexpected end of buffer reading string");
-						return Nan::Null();
+						return;
 					}
 					readString(length, true);
 				} else if (token == 0xda) { //str 16
@@ -103,7 +200,7 @@ public:
 					length += source[position++];
 					if (length + position > size) {
 						Nan::ThrowError("Unexpected end of buffer reading string");
-						return Nan::Null();
+						return;
 					}
 					readString(length, false);
 				} else { //str 32
@@ -113,11 +210,11 @@ public:
 					length += source[position++];
 					if (length + position > size) {
 						Nan::ThrowError("Unexpected end of buffer reading string");
-						return Nan::Null();
+						return;
 					}
 					readString(length, false);
 				}
-				if (writePosition >= MAX_TARGET_SIZE)
+				if (writeSizePosition >= MAX_TARGET_SIZE)
 					break;
 			} else {
 				auto handle = tokenTable[token];
@@ -127,25 +224,32 @@ public:
 					position = tokenTable[token](source, position, size);
 					if (position < 0) {
 						Nan::ThrowError("Unexpected end of buffer");
-						return Nan::Null();	
+						return;
 					}
 				}
 			}
 		}
 
 		if (lastStringEnd)
-			target[writePosition++] = String::NewFromOneByte(isolate, (uint8_t*) source + stringStart, v8::NewStringType::kNormal, lastStringEnd - stringStart).ToLocalChecked();
-#if NODE_VERSION_AT_LEAST(12,0,0)
-		return Array::New(isolate, target, writePosition);
+			sizes[writeSizePosition++] = lastStringEnd;
+/*#if NODE_VERSION_AT_LEAST(12,0,0)
+		return Array::New(isolate, target, writeSizePosition);
 #else
-		Local<Array> array = Array::New(isolate, writePosition);
+		Local<Array> array = Array::New(isolate, writeSizePosition);
 		Local<Context> context = Nan::GetCurrentContext();
-		for (int i = 0; i < writePosition; i++) {
+		for (int i = 0; i < writeSizePosition; i++) {
 			array->Set(context, i, target[i]);
 		}
 		return array;
-#endif
+#endif*/
 	}
+
+	/*void error(FastApiCallbackOptions& options) {
+		if (options)
+			options.fallback = true;
+		else
+			Nan::ThrowError("Unexpected end of buffer reading string");
+	}*/
 };
 void setupTokenTable() {
 	for (int i = 0; i < 256; i++) {
@@ -240,11 +344,24 @@ NAN_METHOD(extractStrings) {
 	Local<Context> context = Nan::GetCurrentContext();
 	int position = Local<Number>::Cast(info[0])->IntegerValue(context).FromJust();
 	int size = Local<Number>::Cast(info[1])->IntegerValue(context).FromJust();
-	if (info[2]->IsArrayBufferView()) {
-		uint8_t* source = (uint8_t*) node::Buffer::Data(info[2]);
-		info.GetReturnValue().Set(extractor->extractStrings(position, size, source));
-	}
+	extractor->extractStrings(position, size);
 }
+
+NAN_METHOD(newExtractStrings) {
+	Local<Context> context = Nan::GetCurrentContext();
+	int position = Local<Number>::Cast(info[0])->IntegerValue(context).FromJust();
+	int size = Local<Number>::Cast(info[1])->IntegerValue(context).FromJust();
+	uint8_t* source = (uint8_t*) node::Buffer::Data(info[2]);
+	info.GetReturnValue().Set(extractor->setupUtf8(source, node::Buffer::Length(info[2])));
+	extractor->extractStrings(position, size);
+}
+NAN_METHOD(setupUtf16) {
+	info.GetReturnValue().Set(extractor->setupUtf16());
+}
+NAN_METHOD(setupSizesBuffer) {
+	info.GetReturnValue().Set(extractor->setupSizesBuffer());
+}
+
 
 NAN_METHOD(isOneByte) {
 	Local<Context> context = Nan::GetCurrentContext();
@@ -254,7 +371,10 @@ NAN_METHOD(isOneByte) {
 void initializeModule(v8::Local<v8::Object> exports) {
 	extractor = new Extractor(); // create our thread-local extractor
 	setupTokenTable();
+	Nan::SetMethod(exports, "newExtractStrings", newExtractStrings);
 	Nan::SetMethod(exports, "extractStrings", extractStrings);
+	Nan::SetMethod(exports, "setupUtf16", setupUtf16);
+	Nan::SetMethod(exports, "setupSizesBuffer", setupSizesBuffer);
 	Nan::SetMethod(exports, "isOneByte", isOneByte);
 }
 
